2501ec7783ce294efffb26d878d6fe93b97b955d
misyaath <mmisyaath@yahoo.co.uk>
2024-10-01 11:11:58 +0530

    refactors

--- app/src/Domain/Files/routes/api.php
+++ app/src/Domain/Files/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\src\Domain\Files\Controllers\TableController;
+use App\src\Domain\Files\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('files')->group(function () {
-        Route::post('/', [TableController::class, 'uploadFile']);
+        Route::post('/', [FileController::class, 'uploadFile']);
     });
 });
