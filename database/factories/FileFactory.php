5068e7357237c47751ccfcaf0692cc94833929e3
misyaath <mmisyaath@yahoo.co.uk>
2024-10-02 16:18:45 +0530

    updated .env.example

---
+++ database/factories/FileFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\src\Domain\Files\Models\File;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class FileFactory extends Factory
+{
+
+    protected $model = File::class;
+
+    public function definition(): array
+    {
+        return [
+            'uuid' => Str::uuid()->toString(),
+            'name' => $this->faker->name,
+            'path' => $this->faker->filePath(),
+            'file_size' => $this->faker->numberBetween()
+        ];
+    }
+}
